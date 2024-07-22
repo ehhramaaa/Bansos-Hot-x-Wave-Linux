5d57d43a9368a8ba4f23b62eb4330c31a6cb3f4c
holycan201 <161028924+holycan201@users.noreply.github.com>
2024-07-22 11:04:53 +0700

    add condition auth_failed

--- core/openvpn.js
+++ core/openvpn.js
@@ -6,10 +6,12 @@ async function startOpenVpn(openVpnPath, ovpnConfig, profileIndex) {
         const openVpnProcess = exec(`doas openvpn --config ${openVpnPath}/${ovpnConfig[profileIndex]} --auth-user-pass ${openVpnPath}/auth.txt --ca ${openVpnPath}/ca.ipvanish.com.crt`);;
 
         openVpnProcess.stdout.on('data', (data) => {
-            prettyConsole('info', data.toString())
             if (data.toString().includes('Initialization Sequence Completed')) {
                 prettyConsole('success', "OpenVPN Started")
                 resolve(true);
+            }
+            if (data.toString().includes("AUTH_FAILED")){
+                prettyConsole('error', "OpenVPN Auth Failed, Please Check Credentials")
             }
         });
 
@@ -24,25 +26,11 @@ async function startOpenVpn(openVpnPath, ovpnConfig, profileIndex) {
 };
 
 async function stopOpenVpn() {
-    return new Promise((resolve, reject) => {
-        const openVpnProcess = exec('killall openvpn')
-
-        openVpnProcess.stdout.on("data", (data) => {
-            if (data.toString().includes('SIGTERM received')) {
-                prettyConsole('success', "OpenVPN Stopped")
-                resolve(true)
-            }
-        })
-
-        openVpnProcess.stderr.on("data", (data) => {
-            prettyConsole('error', data.toString())
-        })
-
-        openVpnProcess.on('error', (error) => {
-            prettyConsole('error', error)
-            reject(false);
-        });
-    })
+    try {
+        exec('killall openvpn')
+    } catch (error) {
+        prettyConsole('error', error.message);
+    }
 };
 
 module.exports = {
