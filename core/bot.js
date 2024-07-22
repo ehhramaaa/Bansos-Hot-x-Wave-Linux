613a92bfd30c5447ac1d698b05402eabc1c5ceb5
holycan201 <161028924+holycan201@users.noreply.github.com>
2024-07-23 02:48:05 +0700

    Fix Selector

--- core/bot.js
+++ core/bot.js
@@ -23,7 +23,7 @@ async function hotWallet(page, threshold) {
     prettyConsole('success', `Account\t:${accountName}`)
 
     // Get Near Balance
-    const nearBalance = await iframeGetText('#root > div > div > div.sc-ifyrTC.kehHNg > div > div > div:nth-child(6) > div:nth-child(2) > div.sc-drnuxz.gogwPM > div:last-child > p:last-of-type', iframe)
+    const nearBalance = parseFloat(await iframeGetText('#root > div > div > div.sc-ifyrTC.kehHNg > div > div > div:nth-child(6) > div:nth-child(2) > div.sc-drnuxz.gogwPM > div:last-child > p:last-of-type', iframe))
 
     if (typeof(nearBalance) === "number") {
         return
